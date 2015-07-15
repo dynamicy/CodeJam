8797e3aa2a2e9b35006a5d43480572016d4e2877
sufrank <su.frank@gmail.com>
2015-07-15 22:58:25 +0800

    Add solution for 136

---
+++ 136_Single_Number/CSharp/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_136_Single_Number
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int nNow = nums[0];
+            for (int i = 1; i < nums.Length; ++i)
+                nNow = nNow ^ nums[i];
+
+            return nNow;
+        }
+    }
+}
