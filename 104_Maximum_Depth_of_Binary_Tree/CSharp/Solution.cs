448f17ebf5167822aac80785fdd6dd6515928a13
sufrank <su.frank@gmail.com>
2015-07-15 22:56:39 +0800

    Add solution for 104

---
+++ 104_Maximum_Depth_of_Binary_Tree/CSharp/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_104_Maximum_Depth_of_Binary_Tree
+{
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            int nLeftDepth = 0;
+            int nRightDepth = 0;
+            if (null == root)
+                return 0;
+            else if (null == root.left && null == root.right)
+                return 1;
+            else
+            {
+                nLeftDepth = MaxDepth(root.left);
+                nRightDepth = MaxDepth(root.right);
+                return ((nLeftDepth >= nRightDepth) ? nLeftDepth : nRightDepth) + 1;
+            }
+        }
+    }
+}
---
+++ 104_Maximum_Depth_of_Binary_Tree/CSharp/TreeNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_104_Maximum_Depth_of_Binary_Tree
+{
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
