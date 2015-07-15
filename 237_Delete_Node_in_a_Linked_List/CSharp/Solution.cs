33395aa97abf52cd3c285cad46f3e10f5a7b449b
sufrank <su.frank@gmail.com>
2015-07-15 23:02:27 +0800

    Add solution for 237 with C#

---
+++ 237_Delete_Node_in_a_Linked_List/CSharp/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_237_Delete_Node_in_a_Linked_List
+{
+    public class Solution
+    {
+        public void DeleteNode(ListNode node)
+        {
+            if (null == node.next)
+            {
+            }
+            else
+            {
+                node.val = node.next.val;
+                node.next = node.next.next;
+            }
+        }
+    }
+}
---
+++ 237_Delete_Node_in_a_Linked_List/CSharp/TreeNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_237_Delete_Node_in_a_Linked_List
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
